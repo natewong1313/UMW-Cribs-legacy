974e607c3259f155f3d0b1867d06f0917905e1e8
Nate <natewong1@GMAIL.COM>
2023-07-05 23:49:44 -0400

    Add recipientEmail field and temporarily remove router

--- email-api/src/worker.ts
+++ email-api/src/worker.ts
@@ -5,37 +5,92 @@ export interface Env {
   EMAIL_API_KEY: string
 }
 
-const router = new Router<Env>()
-router.cors()
+// const router = new Router<Env>()
+// router.cors()
 
-router.use(({ env, req, res, next }) => {
-  if (req.headers.get("x-api-key") !== env.EMAIL_API_KEY) {
-    res.status = 401
-    return
-  }
-  next()
-})
+// router.use(({ env, req, res, next }) => {
+//   if (req.headers.get("x-api-key") !== env.EMAIL_API_KEY) {
+//     res.status = 401
+//     return
+//   }
+//   next()
+// })
 
-router.post("/reset-password", async ({ req, res }) => {
-  if (!req.body.url || req.body.url === "") {
-    res.status = 400
-    res.body = { error: "url is required" }
-    return
-  }
-  const template = resetPasswordTemplate(req.body.url)
-  const emailRequest = buildEmailRequest("Reset Your Password", template)
-  const emailResponse = await fetch(emailRequest)
-  res.status = emailResponse.status
-  res.body = await emailResponse.text()
-})
+// router.post("/reset-password", async ({ req, res }) => {
+//   if (!req.body.url || req.body.url === "") {
+//     res.status = 400
+//     res.body = { error: "url is required" }
+//     return
+//   }
+//   const template = resetPasswordTemplate(req.body.url)
+//   const emailRequest = buildEmailRequest("Reset Your Password", template)
+//   const emailResponse = await fetch(emailRequest)
+//   console.log(emailResponse)
+//   res.status = emailResponse.status
+//   res.body = await emailResponse.text()
+// })
+
+// export default {
+//   async fetch(request: Request, env: Env): Promise<Response> {
+//     return router.handle(env, request)
+//   },
+// }
+
+type RequestBody = {
+  type: string
+  recepientEmail: string
+  url?: string
+}
 
 export default {
-  async fetch(request: Request, env: Env): Promise<Response> {
-    return router.handle(env, request)
+  async fetch(req: Request, env: Env) {
+    if (req.headers.get("x-api-key") !== env.EMAIL_API_KEY) {
+      return new Response(null, { status: 401 })
+    }
+    if (req.method !== "POST") {
+      return new Response(null, { status: 405 })
+    }
+
+    const body: RequestBody = await req.json()
+    if (!body) {
+      return new Response(null, { status: 400 })
+    }
+    if (!body.recepientEmail || body.recepientEmail === "") {
+      return new Response(
+        JSON.stringify({ error: "recepient email is required" }),
+        { status: 400 }
+      )
+    }
+
+    let emailRequest
+    switch (body.type) {
+      case "reset-password":
+        if (!body.url || body.url === "") {
+          return new Response(JSON.stringify({ error: "url is required" }), {
+            status: 400,
+          })
+        }
+        emailRequest = buildEmailRequest(
+          "Reset Your Password",
+          body.recepientEmail,
+          resetPasswordTemplate(body.url)
+        )
+        break
+      default:
+        return new Response("Unknown type", { status: 400 })
+    }
+    const emailResponse = await fetch(emailRequest)
+    return new Response(await emailResponse.text(), {
+      status: emailResponse.status,
+    })
   },
 }
 
-const buildEmailRequest = (subject: string, template: string) =>
+const buildEmailRequest = (
+  subject: string,
+  recepientEmail: string,
+  template: string
+) =>
   new Request("https://api.mailchannels.net/tx/v1/send", {
     method: "POST",
     headers: {
@@ -43,7 +98,7 @@ const buildEmailRequest = (subject: string, template: string) =>
     },
     body: JSON.stringify({
       personalizations: [
-        { to: [{ email: "natewong1@gmail.com", name: "Test Recipient" }] },
+        { to: [{ email: recepientEmail, name: "Recipient" }] },
       ],
       from: {
         email: "admin@umwcribs.com",
