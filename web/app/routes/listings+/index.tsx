ee1f1be0e8091a98a8ed3d4ce5ecad53bebb093b
Nate <natewong1@GMAIL.COM>
2023-07-25 13:50:17 -0400

    Add liked listings functionality

--- web/app/routes/listings+/index.tsx
+++ web/app/routes/listings+/index.tsx
@@ -1,34 +1,76 @@
-import { json, type LoaderArgs } from "@remix-run/cloudflare"
-import { Link, useLoaderData } from "@remix-run/react"
-import { listing } from "@umw-cribs/db/schema.server"
+import { ActionArgs, json, type LoaderArgs } from "@remix-run/cloudflare"
+import { Form, Link, useLoaderData } from "@remix-run/react"
+import { listing, userLikedListings } from "@umw-cribs/db/schema.server"
+import { eq } from "drizzle-orm"
 
 export const loader = async ({ request, context }: LoaderArgs) => {
+  const headers = new Headers()
   const listings = await context.db.select().from(listing)
-  return json({ listings })
+  const authRequest = context.auth.handleRequest(request, headers)
+  const { user } = await authRequest.validateUser()
+  if (!user) return json({ listings, likedListingIds: [] }, { headers })
+  const likedListings = await context.db
+    .select()
+    .from(userLikedListings)
+    .where(eq(userLikedListings.userId, user.userId))
+  return json(
+    { listings, likedListingIds: likedListings.map((l) => l.listingId) },
+    { headers }
+  )
+}
+
+export const action = async ({ request, context }: ActionArgs) => {
+  const headers = new Headers()
+  const body = await request.formData()
+  const listingId = body.get("id")
+  if (!listingId) return json(null, { headers })
+  const authRequest = context.auth.handleRequest(request, headers)
+  const { user } = await authRequest.validateUser()
+  if (!user) return json(null, { headers })
+  console.log(user.userId)
+
+  const hasAlreadyLiked = await context.db
+    .select()
+    .from(userLikedListings)
+    .where(eq(userLikedListings.userId, user.userId))
+    .where(eq(userLikedListings.listingId, listingId.toString()))
+  if (hasAlreadyLiked.length === 0) {
+    console.log("OK")
+    await context.db.insert(userLikedListings).values({
+      userId: user.userId,
+      listingId: listingId.toString(),
+    })
+  }
+  return json(null, { headers })
 }
 
 export default function ListingsPage() {
-  const { listings } = useLoaderData<typeof loader>()
-  const likePost = (listingId: string) => {}
+  const { listings, likedListingIds } = useLoaderData<typeof loader>()
   return (
     <div>
       ListingsPage
       <ul>
         {listings.map((listing) => (
-          <li
-            key={listing.id}
-            className="flex flex-row items-center gap-10 space-y-2 border"
-          >
-            <Link to={`/listings/${listing.id}`}>
-              {listing.addressLineOne} {listing.addressLineTwo}{" "}
-              {listing.listingSource}
-            </Link>
-            <button
-              onClick={() => likePost(listing.id)}
-              className="rounded bg-blue-500 px-2 py-1 text-white"
+          <li key={listing.id}>
+            <Form
+              method="post"
+              className="flex flex-row items-center gap-10 space-y-2 border"
             >
-              Like
-            </button>
+              <Link to={`/listings/${listing.id}`}>
+                {listing.addressLineOne} {listing.addressLineTwo}{" "}
+                {listing.listingSource}
+              </Link>
+              <input name="id" type="hidden" value={listing.id} />
+              {likedListingIds.includes(listing.id) ? (
+                <button className="rounded bg-gray-500 px-2 py-1 text-white">
+                  Unlike
+                </button>
+              ) : (
+                <button className="rounded bg-blue-500 px-2 py-1 text-white">
+                  Like
+                </button>
+              )}
+            </Form>
           </li>
         ))}
       </ul>
